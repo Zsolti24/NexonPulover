654af89af227da5c5732f5566ef6051a9a921463
Zsolti24 <tothzsolt2004@gmail.com>
2024-08-06 12:03:12 +0200

    Done

--- src/components/DraggableComponent.jsx
+++ src/components/DraggableComponent.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import { useDraggable } from '@dnd-kit/core';
-import { CSS } from '@dnd-kit/utilities';
 
 function DraggableComponent({ id, src, alt }) {
   const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
     id,
   });
 
-  const style = {
-    touchAction: 'none',
-    width: '130px', 
-    display:'flex',
-    marginBottom:'-2px',
-    marginTop:'-4px',
-    opacity: isDragging ? 1 : 1, 
-    zIndex: isDragging ? 1000 : 100,
-    cursor: isDragging ? 'grab' : 'pointer',
-
-    transform: isDragging ? `translate(${transform.x}px, ${transform.y}px)` : 'none', // Csak a pozíció
-  };
+  // Az osztályok dinamikus beállítása
+  const className = `draggable ${isDragging ? 'dragging' : ''}`;
 
   return (
-    <div ref={setNodeRef} style={style} {...listeners} {...attributes} >
-      <img src={src} alt={alt} className='coatImg'/>
+    <div
+      ref={setNodeRef}
+      className={className}
+      style={{
+        transform: isDragging ? `translate(${transform.x}px, ${transform.y}px)` : 'none', // Csak a pozíció
+      }}
+      {...listeners}
+      {...attributes}
+    >
+      <img src={src} alt={alt} className='coatImg' />
     </div>
   );
 }
